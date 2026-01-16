66f265247cbc12db95486ccbd3c6fbf436739754
azi <azi@fiftytwo.com>
2026-01-16 15:35:38 +0600

    feat: add Login page and routing for authentication

---
+++ src/pages/Login.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import api from "../api/api";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/api/login", {
+        email,
+        password,
+      });
+
+      // Sanctum returns token
+      localStorage.setItem("token", response.data.token);
+
+      alert("Login successful!");
+    } catch (err) {
+      setError("Invalid email or password");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Jobs from "../pages/Jobs";
 import CreateJob from "../pages/CreateJob";
+import Login from "../pages/Login";
 
 export default function AppRoutes() {
     return (
@@ -9,12 +10,14 @@ export default function AppRoutes() {
             <nav>
                 <Link to="/">Dashboard</Link> | {" "}
                 <Link to="/jobs">Jobs</Link> | {" "}
-                <Link to="/create-jobs">Create Job</Link>
+                <Link to="/create-job">Create Job</Link>| {" "}
+                <Link to="/login">Login</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/jobs" element={<Jobs />} />
                 <Route path="/create-job" element={<CreateJob />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
