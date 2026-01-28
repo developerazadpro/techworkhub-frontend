05e0aac793d5f406818c0ba4062dd366833e2d1c
azi <azharul.ece13.hstu@gmail.com>
2026-01-28 15:08:22 +0600

    feat: improve job matching and editing flow
    - Re-run Go matching service on job update
    - Persist updated recommended_technicians
    - Add Edit Job page with inline success feedback
    - Extract normalizeSkills into shared utility
    - Standardize route naming in AppRoutes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,12 +1,8 @@
-// AppRoutes.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BaseLayout from "../layouts/BaseLayout";
-
 
 // Auth
 import Login from "../pages/Login";
 import RootRedirect from "./RootRedirect";
-//import ProtectedRoute from "./ProtectedRoute";
 
 // Layouts 
 import TechnicianLayout from "../layouts/TechnicianLayout";
@@ -14,15 +10,16 @@ import ClientLayout from "../layouts/ClientLayout";
 
 // Technician pages
 import TechnicianDashboard from "../pages/technician/Dashboard";
-import Jobs from "../pages/technician/Jobs";
-import JobDetails from "../pages/technician/JobDetails";
-import MyJobs from "../pages/technician/MyJobs";
+import TechnicianJobs from "../pages/technician/Jobs";
+import TechnicianJobDetails from "../pages/technician/JobDetails";
+import TechnicianMyJobs from "../pages/technician/MyJobs";
 import TechnicianProfile from "../pages/technician/Profile";
 
 // Client pages
 import ClientDashboard from "../pages/client/Dashboard";
-import CreateJob from "../pages/client/CreateJob";
-import ClientMyJobs from "../pages/client/ClientMyJobs";
+import ClientCreateJob from "../pages/client/CreateJob";
+import ClientEditJob from "../pages/client/EditJob";
+import ClientMyJobs from "../pages/client/MyJobs";
 import ClientProfile from "../pages/client/Profile";
 
 export default function AppRoutes() {
@@ -37,25 +34,25 @@ export default function AppRoutes() {
         
 
         {/* App layout */}
-        {/* <Route element={ <ProtectedRoute> <BaseLayout /> </ProtectedRoute>} > */}
+        
           {/* Technician */}
           <Route path="/technician" element={<TechnicianLayout />}>
             <Route index element={<TechnicianDashboard />} />
-            <Route path="jobs" element={<Jobs />} />
-            <Route path="job/:id" element={<JobDetails />} />
-            <Route path="my-jobs" element={<MyJobs />} />
+            <Route path="jobs" element={<TechnicianJobs />} />
+            <Route path="job/:id" element={<TechnicianJobDetails />} />
+            <Route path="my-jobs" element={<TechnicianMyJobs />} />
             <Route path="profile" element={<TechnicianProfile />} />
           </Route>
 
           {/* Client */}
           <Route path="/client" element={<ClientLayout />}>
             <Route index element={<ClientDashboard />} />
-            <Route path="create-job" element={<CreateJob />} />
+            <Route path="create-job" element={<ClientCreateJob />} />
+            <Route path="jobs/:id/edit" element={<ClientEditJob />} />
             <Route path="my-jobs" element={<ClientMyJobs />} />
             <Route path="profile" element={<ClientProfile />} />
-          </Route>
-          
-        {/* </Route> */}
+          </Route>         
+        
       </Routes>
     </BrowserRouter>
   );
